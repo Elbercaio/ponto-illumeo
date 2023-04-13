ab9f3a97511882a982d376150f2b550dbd1f9cde
Elber Lopes <elbercaio@gmail.com>
2023-04-13 18:56:06 -0300

    Api first commit

---
+++ api/src/index.ts
@@ -0,0 +1,23 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Request, Response } from 'express';
+import { port } from './config';
+import usersRoutes from './routes/users.routes';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', usersRoutes);
+
+app.get('/', async (_: Request, res: Response) => {
+  res.status(200).json({
+    message: 'Hello World',
+  });
+});
+
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`API started at http://localhost:${port}`);
+});
